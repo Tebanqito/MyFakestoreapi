ae0a8fa3b0ecbd95f164bd6e4f2f8bf6956e49f3
Tebanqito <yemesesteban@gmail.com>
2023-06-10 03:07:05 -0300

    getUserById

--- api/src/store/controllers/UserController.ts
+++ api/src/store/controllers/UserController.ts
@@ -1,17 +1,19 @@
 import { v4 as uuidv4 } from "uuid";
-import {
-  UserInput,
-  User,
-} from "../models/User";
+import { UserInput, User } from "../models/User";
 import { Product } from "../models/Product";
 
 export const createUser = async (user: UserInput): Promise<User> => {
-    const id = uuidv4();
-    const createdUser = await User.create({ ...user, id });
-    return createdUser;
+  const id = uuidv4();
+  const createdUser = await User.create({ ...user, id });
+  return createdUser;
 };
 
 export const getAllUsers = async (): Promise<User[]> => {
-    const users = await User.findAll({ include: [{ model: Product }] });
-    return users;
+  const users = await User.findAll({ include: [{ model: Product }] });
+  return users;
+};
+
+export const getUserById = async (id: string): Promise<User | null> => {
+  const user = await User.findByPk(id, { include: [Product] });
+  return user;
 };
