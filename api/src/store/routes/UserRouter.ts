7c961610e89c518da2934e359e2fe76d4e42528d
Tebanqito <yemesesteban@gmail.com>
2023-06-14 19:46:27 -0300

    GET /users/:id

--- api/src/store/routes/UserRouter.ts
+++ api/src/store/routes/UserRouter.ts
@@ -1,10 +1,10 @@
 import { Router, Request, Response } from "express";
 import {
-    createUser,
-    getAllUsers,
-    getUserById,
-    updateUserById,
-    deleteUserById,
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById,
 } from "../controllers/UserController";
 import { UserAttributes } from "../models/User";
 import axios from "axios";
@@ -12,13 +12,36 @@ import axios from "axios";
 const userRouter = Router();
 
 userRouter.get("/", async (req: Request, res: Response) => {
-    try {
-      const users = await getAllUsers();
-      res.status(200).json(users);
-    } catch (error) {
-      console.error(error);
-      res.status(400).json({ error: "error al obtener todos los usuarios.", route: "/" });
-    }
+  try {
+    const users = await getAllUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(400)
+      .json({ error: "error al obtener todos los usuarios.", route: "/" });
+  }
+});
+
+userRouter.get("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    if (!id) throw new Error("El id del paciente no esta indefinido.");
+
+    const user = await getUserById(id);
+    if (!user) throw new Error(`El usuario con el id ${id} no se encuentra en la BDD.`);
+    
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(400)
+      .json({
+        error: "error al obtener todos los usuarios.",
+        route: "/users/:id",
+      });
+  }
 });
 
 export default userRouter;
