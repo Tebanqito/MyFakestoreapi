eb0fed41bd2eb5aba103c52a9b4e5f81ef14d4ad
Tebanqito <yemesesteban@gmail.com>
2023-06-14 19:49:12 -0300

    users enrutado

--- api/src/store/routes/index.ts
+++ api/src/store/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import productRouter from './ProductRouter';
+import userRouter from './UserRouter';
 
 const router = Router();
 
 router.use("/products", productRouter);
+router.use("/users", userRouter);
 
 export default router;
