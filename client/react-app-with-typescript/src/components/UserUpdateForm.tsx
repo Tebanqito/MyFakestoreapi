f32627f184c18e6cd76cd508ce58317a433d1654
Tebanqito <yemesesteban@gmail.com>
2023-06-16 18:07:55 -0300

    formulario para actualizar un usuario

---
+++ client/react-app-with-typescript/src/components/UserUpdateForm.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { updateUser } from '../store/UserSlice';
+import { AppDispatch } from '../store';
+
+interface UserUpdateFormProps {
+    userId: string;
+};
+
+const UserUpdateForm: React.FC<UserUpdateFormProps> = ({ userId }) => {
+    const dispatch = useDispatch<AppDispatch>();
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState('');
+
+    const handleFormSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const updatedFields = {
+            id: userId,
+            attributes: {
+                ...(name && { name }),
+                ...(age && { age: parseFloat(age) }),
+                ...(description && { description }),
+                ...(image && { image }),
+            },
+        };
+        
+        dispatch(updateUser(updatedFields));
+    };
+
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <label>
+                Name:
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            </label>
+            <label>
+                Age:
+                <input type="number" value={age} onChange={(e) => setAge(e.target.value)} />
+            </label>
+            <label>
+                Description:
+                <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+            </label>
+            <label>
+                Image:
+                <input type="text" value={image} onChange={(e) => setImage(e.target.value)} />
+            </label>
+            <button type="submit">Update User</button>
+        </form>
+    );
+};
+
+export default UserUpdateForm;
