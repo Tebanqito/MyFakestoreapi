b2670993ad8e04da83af92cbae2b4de3e5bd9ae7
Tebanqito <yemesesteban@gmail.com>
2023-06-17 01:39:02 -0300

    asociacion de User con Product

--- api/src/config/database.config.ts
+++ api/src/config/database.config.ts
@@ -1,9 +1,14 @@
 import { Sequelize } from 'sequelize';
+import { User } from '../store/models/User';
+import { Product } from '../store/models/Product';
 
 const db = new Sequelize('app', '', '', {
 	storage: './database.sqlite',
 	dialect: 'sqlite',
 	logging: false,
 });
+
+User.hasMany(Product);
+Product.belongsTo(Product);
 
 export default db;
