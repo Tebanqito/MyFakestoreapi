d6360caf1a75ac7a4b0f5ab865daa2914c1d6b8e
Tebanqito <yemesesteban@gmail.com>
2023-06-17 01:43:09 -0300

    asociacion entre User y Product removida

--- api/src/config/database.config.ts
+++ api/src/config/database.config.ts
@@ -1,14 +1,9 @@
 import { Sequelize } from 'sequelize';
-import { User } from '../store/models/User';
-import { Product } from '../store/models/Product';
 
 const db = new Sequelize('app', '', '', {
 	storage: './database.sqlite',
 	dialect: 'sqlite',
 	logging: false,
 });
-
-User.hasMany(Product);
-Product.belongsTo(Product);
 
 export default db;
