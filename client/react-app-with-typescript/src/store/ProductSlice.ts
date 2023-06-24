21608e091bc1a51e95c5ac889848bc9552a9b87f
Tebanqito <yemesesteban@gmail.com>
2023-06-24 04:11:07 -0300

    getProductById en ProductSlice

--- client/react-app-with-typescript/src/store/ProductSlice.ts
+++ client/react-app-with-typescript/src/store/ProductSlice.ts
@@ -1,17 +1,22 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { Product } from "../types/types";
+import { createSlice } from "@reduxjs/toolkit";
+import { Product, User } from "../types/types";
 import { RootState } from "../store";
-import { fetchProducts } from "./actions/ProductActions";
+import {
+  fetchProducts,
+  getProductById,
+} from "./actions/ProductActions";
 
 interface ProductState {
   products: Product[];
+  product: Partial<Product>;
+  ownUser: Partial<User>;
   loading: boolean;
   error: string | null;
 }
 
 const productSlice = createSlice({
   name: "products",
-  initialState: { products: [], loading: false, error: null } as ProductState,
+  initialState: { products: [], loading: false, error: null, ownUser: {}, product: {} } as ProductState,
   reducers: {},
   extraReducers: (builder) => {
     builder
@@ -24,6 +29,18 @@ const productSlice = createSlice({
         state.products = action.payload;
       })
       .addCase(fetchProducts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message ?? "Error desconocido";
+      })
+      .addCase(getProductById.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getProductById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.product = action.payload;
+      })
+      .addCase(getProductById.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message ?? "Error desconocido";
       });
