b18d2c2856fb1bdcb82896fdcecb6b85219f8d9e
Tebanqito <yemesesteban@gmail.com>
2023-06-24 00:42:51 -0300

    configuracion de db

--- api/src/config/database.config.ts
+++ api/src/config/database.config.ts
@@ -1,33 +1,15 @@
 import { Sequelize } from "sequelize-typescript";
-// import { User } from "../store/models/User";
-// import { Product } from "../store/models/Product";
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+import dotenv from 'dotenv';
 
-// const db = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/fackestoreapi`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+dotenv.config();
 
-const db = new Sequelize(`postgres://postgres:admin@localhost:5432/fackestoreapi`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  models: [__dirname + '/models/**/*.model.ts'],
-  modelMatch: (filename, member) => {
-    return filename.substring(0, filename.indexOf('.model')) === member.toLowerCase();
-  },
+const DB_USER: string = process.env.DB_USER || "";
+const DB_PASSWORD: string = process.env.DB_PASSWORD || "";
+const DB_HOST: string = process.env.DB_HOST || "";
+
+const db = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/fackestoreapi`, {
+  logging: false,
+  native: false,
 });
-
-db.addModels([__dirname + '../models/User.ts']);
-db.addModels([__dirname + '../models/Product.ts']);
-
-// User.associate({ Product });
-// Product.associate({ User });
-
-// User.hasMany(Product, {
-//   sourceKey: 'id',
-//   foreignKey: 'ownerId',
-//   as: 'products' // this determines the name in `associations`!
-// });
-// Product.belongsTo(User, { targetKey: 'id' });
 
 export default db;
