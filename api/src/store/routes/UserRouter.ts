b1f04d7e111d357ca555ee5ea0945182d2c6a99a
Tebanqito <yemesesteban@gmail.com>
2023-06-23 23:21:30 -0300

    GET /api/users/linkProduct/:id funcionando con exito

--- api/src/store/routes/UserRouter.ts
+++ api/src/store/routes/UserRouter.ts
@@ -3,15 +3,18 @@ import {
   createUser,
   getAllUsers,
   getUserById,
-  updateUserById,
-  deleteUserById,
+  // updateUserById,
+  // deleteUserById,
+  linkProduct
 } from "../controllers/UserController";
+import { User, UserNoPassword } from "../models/user.model";
+import { Product } from "../models/product.model";
 
 const userRouter = Router();
 
 userRouter.get("/", async (req: Request, res: Response) => {
   try {
-    const users = await getAllUsers();
+    const users: User[] = await getAllUsers();
     res.status(200).json(users);
   } catch (error) {
     console.error(error);
@@ -22,12 +25,12 @@ userRouter.get("/", async (req: Request, res: Response) => {
 });
 
 userRouter.get("/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
+  const id: string = req.params.id;
 
   try {
     if (!id) throw new Error("El id del paciente no esta indefinido.");
 
-    const user = await getUserById(parseInt(id));
+    const user: UserNoPassword = await getUserById(id);
     if (!user)
       throw new Error(`El usuario con el id ${id} no se encuentra en la BDD.`);
 
@@ -45,48 +48,65 @@ userRouter.post("/", async (req: Request, res: Response) => {
   const userToCreate = req.body;
 
   try {
-    const user = await createUser(userToCreate);
-    res.status(200).json(user);
+    const user: User = await createUser(userToCreate);
+    if (!user) throw new Error();
+
+    res.status(200).json({ success: "Usuario creado con exito." });
   } catch (error) {
     console.error(error);
     res.status(400).json({ error: "error al crear un producto.", route: "/" });
   }
 });
 
-userRouter.put("/update/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const attributes = req.body;
+userRouter.post("/linkProduct/:id", async (req: Request, res: Response) => {
+  const userId: string = req.body.userId;
+  const productId: string = req.params.id;
 
   try {
-    const user = await updateUserById(parseInt(id), attributes);
-    if (!user)
-      throw new Error(`El usuario con el id ${id} no se encuentra en la BDD.`);
+    const user: UserNoPassword = await linkProduct(userId, productId);
+    if(!user) throw new Error();
 
     res.status(200).json(user);
   } catch (error) {
     console.error(error);
-    res.status(400).json({
-      error: "error al actualizar un usuario.",
-      route: "/update/:id",
-    });
+    res.status(400).json({ error: "error al comprar un producto.", route: "/" });
   }
 });
 
-userRouter.delete("/delete/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
+// userRouter.put("/update/:id", async (req: Request, res: Response) => {
+//   const id: string = req.params.id;
+//   const attributes = req.body;
 
-  try {
-    const user = await deleteUserById(parseInt(id));
-    if (!user)
-      throw new Error(`El usuario con el id ${id} no se encuentra en la BDD.`);
+//   try {
+//     const user = await updateUserById(id, attributes);
+//     if (!user)
+//       throw new Error(`El usuario con el id ${id} no se encuentra en la BDD.`);
 
-    res.status(200).json(user);
-  } catch (error) {
-    console.error(error);
-    res
-      .status(400)
-      .json({ error: "error al eliminar un usuario.", route: "/delete/:id" });
-  }
-});
+//     res.status(200).json(user);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(400).json({
+//       error: "error al actualizar un usuario.",
+//       route: "/update/:id",
+//     });
+//   }
+// });
+
+// userRouter.delete("/delete/:id", async (req: Request, res: Response) => {
+//   const id: string = req.params.id;
+
+//   try {
+//     const user = await deleteUserById(id);
+//     if (!user)
+//       throw new Error(`El usuario con el id ${id} no se encuentra en la BDD.`);
+
+//     res.status(200).json(user);
+//   } catch (error) {
+//     console.error(error);
+//     res
+//       .status(400)
+//       .json({ error: "error al eliminar un usuario.", route: "/delete/:id" });
+//   }
+// });
 
 export default userRouter;
