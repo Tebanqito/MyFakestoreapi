53d632bf44a594ac3e54153b488cf7772d94595a
Tebanqito <yemesesteban@gmail.com>
2023-06-25 03:11:16 -0300

    obteniendo user correctamente

--- client/react-app-with-typescript/src/components/LoginUser.tsx
+++ client/react-app-with-typescript/src/components/LoginUser.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { loginUser } from '../store/actions/AuthActions';
 import { AppDispatch } from '../store';
+import { selectUsers } from '../store/UserSlice';
 
 const LoginUser: React.FC = () => {
     const dispatch = useDispatch<AppDispatch>();
     const [userName, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const user = useSelector(selectUsers).user;
 
     const handleLogin = () => {
         dispatch(loginUser({ userName, password }));
@@ -14,19 +16,21 @@ const LoginUser: React.FC = () => {
 
     return (
         <div>
-            <input
-                type="text"
-                placeholder="Username"
-                value={userName}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={handleLogin}>Login</button>
+            {!user && <div>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={userName}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button onClick={handleLogin}>Login</button>
+            </div>}
         </div>
     );
 };
