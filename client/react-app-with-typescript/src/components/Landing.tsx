a995997635db98a28ecf24329324449421b833be
Tebanqito <yemesesteban@gmail.com>
2023-06-25 03:15:42 -0300

    componente Landing

---
+++ client/react-app-with-typescript/src/components/Landing.tsx
@@ -0,0 +1,27 @@
+import { useSelector } from 'react-redux';
+import RegisterUser from './RegisterUser';
+import LoginUser from './LoginUser';
+import { RootState } from '../store';
+import Home from './Home';
+import { useState } from 'react';
+
+const Landing: React.FC = () => {
+    const user = useSelector((state: RootState) => state.auth.user);
+    const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+    const [isRegister, setIsRegister] = useState<boolean>(false);
+
+    return (
+        <div className="App">
+            {isAuthenticated ? <Home user={user} /> :
+                <div>
+                    <button onClick={() => setIsRegister(!isRegister)}>
+                        {!isRegister ? "Iniciar Sesion" : "Registrarse"}
+                    </button>
+                    {!isRegister ? <RegisterUser /> : <LoginUser /> }
+                </div>
+            }
+        </div>
+    );
+}
+
+export default Landing;
