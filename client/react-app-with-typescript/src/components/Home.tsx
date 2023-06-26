c4f0a7bfa50e525710f378e1c74f50d1fa8ea459
Tebanqito <yemesesteban@gmail.com>
2023-06-26 12:42:46 -0300

    UserUpdateForm en Home

--- client/react-app-with-typescript/src/components/Home.tsx
+++ client/react-app-with-typescript/src/components/Home.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../store';
 import { User } from '../types/types';
 import { logout } from '../store/AuthSlice';
+import UserUpdateForm from './UserUpdateForm';
 
 interface HomeProps {
     user: Partial<User> | null;
@@ -10,20 +11,24 @@ interface HomeProps {
 
 const Home: React.FC<HomeProps> = ({ user }) => {
     const dispatch = useDispatch<AppDispatch>();
+    const [isUpdateUser, setIsUpdateUser] = useState<boolean>(false);
 
-
-    const handleLogout = () => {
+    const handleLogout = (): void => {
         dispatch(logout());
     };
 
     return (
         <div>
-            <button onClick={handleLogout}>logout</button>
-            <button>Atualizar Perfil</button>
-            <div>
-                <h1>Home</h1>
-                <p>Hola {user?.name}</p>
-            </div>
+            <button onClick={() => setIsUpdateUser(!isUpdateUser)}>
+                {isUpdateUser ? "Home" : "Actualizar perfil"}
+            </button>
+            <button onClick={handleLogout}>Logout</button>
+            {!isUpdateUser ?
+                <div>
+                    <h1>Home</h1>
+                    <p>Hola {user?.name}</p>
+                </div>
+                : <UserUpdateForm userId={user?.id as string} />}
         </div>
     );
 };
